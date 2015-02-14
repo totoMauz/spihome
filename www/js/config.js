a7220c5eb4c5c43d8da853220363b5fee63f5c67
Torsten Abraham <torsten.abraham1+github@gmail.com>
2015-02-14 14:37:23 +0100

    Creaty empty pages dynamically

--- www/js/config.js
+++ www/js/config.js
@@ -4,41 +4,39 @@
 
     //public
     sPh.readConfig = function () {
-	$.getJSON("config.json", function(data) {
-	    sPh.clearMenu();
-	    Object.keys(data).forEach(function (categoryKey) {
-		var oCategory = data[categoryKey];
-		sPh.createMenuButton(oCategory.name, oCategory.site);
-		sPh.createPage(oCategory);
+	    $.getJSON("config.json", function(data) {
+	        sPh.clearMenu();
+	        Object.keys(data).forEach(function (categoryKey) {
+		        var oCategory = data[categoryKey];
+		        sPh.createMenuButton(oCategory.name, oCategory.site);
+		        sPh.createPage(oCategory);
+	        });
+	    }).fail(function() {
+	        console.error("Coudn't read config");
+	    }).done(function() {
+	        sPh.addNavigation($('[name="btnMenu"]'), '#menu', true);
 	    });
-	}).fail(function() {
-	    console.error("Coudn't read config");
-	}).done(function() {
-	    sPh.addNavigation($('[name="btnMenu"]'), '#menu', true);
-	});
     };
 
     sPh.createPage = function (oCategory) {
-	//TODO generalize
-	if(oCategory.site !== "#dummy"){return;}
-
-	var header = "<div data-role='header' data-position='fixed'><button class='ui-btn-left ui-btn ui-btn-inline ui-mini ui-corner-all ui-btn-icon-right' name='btnMenu' id='dummyMenu'>Menu</button><h1>Dummy</h1>",
+        var pageId = oCategory.site.charAt(0) === '#' ? oCategory.site.slice(1) : oCategory.site;
+	    var header = String.format("<div data-role='header' data-position='fixed'><button class='ui-btn-left ui-btn ui-btn-inline ui-mini ui-corner-all ui-btn-icon-right' name='btnMenu' id='{0}Menu'>Menu</button><h1>{1}</h1>", pageId, oCategory.name),
         body = "",
-	footer = "";
-	var page = $("<div data-role='page' id='dummy'>" + header+body+footer + "</div>");
-	page.appendTo($.mobile.pageContainer);
-	console.debug("Created page dummy");
+	    footer = "";
+	    var page = $(String.format("<div data-role='page' id='{0}'>{1}{2}{3}</div>", pageId, header, body, footer));
+	    page.appendTo($.mobile.pageContainer);
+	    console.debug("Created page " + oCategory.site);
     };
 
     sPh.createMenuButton = function(sCategory, sSite) {
-	var sId = "btn" + sCategory;
-	$menu.append('<span data-role="button" id="' + sId + '" >' + sCategory + '</span>');
-	$menu.find("span[data-role='button']").button();
-	sPh.addNavigation($('#'+sId), sSite);
+	    var sId = "btn" + sCategory;
+	    $menu.append('<span data-role="button" id="' + sId + '" >' + sCategory + '</span>');
+	    $menu.find("span[data-role='button']").button();
+	    sPh.addNavigation($('#'+sId), sSite);
     };
 
     sPh.clearMenu = function () {
-	$("span[data-role='button']", "#menu").remove();
+	    $("span[data-role='button']", "#menu").remove();
     };
 
     //execute this on start up
--- www/js/navigation.js
+++ www/js/navigation.js
@@ -1,27 +1,27 @@
 (function(sPh, $, undefined) {
     sPh.addNavigation = function ($selector, sSite, isReverse) {
-	console.debug("attached click event to " + $selector.map(function() {return this.id;}).get());
+	    console.debug("attached click event to " + $selector.map(function() {return this.id;}).get());
 
-	//if it's not a button, assume it's one of the generated ones - get it's 'div button' parent
-	var $eventSource = $selector.is("button") ? $selector : $selector.parent('.ui-btn');
+	    //if it's not a button, assume it's one of the generated ones - get it's 'div button' parent
+	    var $eventSource = $selector.is("button") ? $selector : $selector.parent('.ui-btn');
 
-	$eventSource.click(function (evt) {
-	    console.debug("click event from " + (evt.target.id || evt.target.children[0].id));
-	    $.mobile.pageContainer.pagecontainer('change', sSite, {
-		transition: 'slide',
-		changeHash: true,
-		reverse: isReverse || false,
-		showLoadMsg: true
+	    $eventSource.click(function (evt) {
+	        console.debug("click event from " + (evt.target.id || evt.target.children[0].id));
+	        $.mobile.pageContainer.pagecontainer('change', sSite, {
+		        transition: 'slide',
+		        changeHash: true,
+		        reverse: isReverse || false,
+		        showLoadMsg: true
+	        });
 	    });
-	});
     };
 
     sPh.addPageContainerListener = function (){
-	$.mobile.pageContainer.on( "pagecontainerchangefailed", function( event, ui ) {
-	    console.error("Coudn't change page to " + ui.toPage);
-	});
-	$.mobile.pageContainer.on( "pagecontainerchange", function( event, ui ) {
-	    console.debug("Page changed to " + ui.toPage.map(function() {return this.id;}).get());
-	});
+	    $.mobile.pageContainer.on( "pagecontainerchangefailed", function( event, ui ) {
+	        console.error("Coudn't change page to " + ui.toPage);
+	    });
+	    $.mobile.pageContainer.on( "pagecontainerchange", function( event, ui ) {
+	        console.debug("Page changed to " + ui.toPage.map(function() {return this.id;}).get());
+	    });
     };
 }(window.sPh = window.sPh || {}, jQuery));
