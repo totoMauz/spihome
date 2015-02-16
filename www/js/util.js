e1b8eea9e24a02ee838d514c6fe07e9010cfb532
Torsten Abraham <torsten.abraham1+github@gmail.com>
2015-02-16 22:41:05 +0100

    Some lintness

--- www/js/util.js
+++ www/js/util.js
@@ -1,12 +1,12 @@
-(function(sPh, $, undefined) {
+(function (sPh, $, undefined) {
+    "use strict";
     if (!String.format) {
-        String.format = function(format) {
+        String.format = function (format) {
             var args = Array.prototype.slice.call(arguments, 1);
-            return format.replace(/{(\d+)}/g, function(match, number) {
-                return typeof args[number] !== 'undefined'
+            return format.replace(/{(\d+)}/g, function (match, number) {
+                return args[number] !== undefined
                     ? args[number]
-                    : match
-                ;
+                    : match;
             });
         };
         console.debug("Created String.format");
@@ -14,23 +14,23 @@
 
     function dynamicSort(property) {
         var sortOrder = 1;
-        if(property[0] === "-") {
+        if (property[0] === "-") {
             sortOrder = -1;
             property = property.substr(1);
         }
-        return function (a,b) {
+        return function (a, b) {
             var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
             return result * sortOrder;
-        }
+        };
     }
 
     Object.defineProperty(Array.prototype, "sortBy", {
         enumerable: false,
         writable: true,
-        value: function() {
+        value: function () {
             return this.sort(dynamicSort.apply(null, arguments));
         }
-    })
+    });
 
-    console.debug("Created Array.sortBy");;
+    console.debug("Created Array.sortBy");
 }(window.sPh = window.sPh || {}, jQuery));
